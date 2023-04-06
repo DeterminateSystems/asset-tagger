5f25b09deb355b16bdfcebb2fc0be252d076d682
Graham Christensen <graham@grahamc.com>
2023-04-05 21:08:07 -0400

    format and update

--- flake.nix
+++ flake.nix
@@ -3,15 +3,9 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs =
-    { self
-    , nixpkgs
-    , ...
-    } @ inputs:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
+  outputs = { self, nixpkgs, ... }@inputs:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
       devShells.x86_64-linux.default = pkgs.mkShell {
         name = "asset";
 
@@ -27,12 +21,17 @@
       };
 
       packages.x86_64-linux.default = pkgs.resholve.writeScript "name" {
-        inputs = with pkgs; [ curl jq coreutils qrcode imagemagick 
-          python3.pkgs.brother-ql ];
-        interpreter = "${pkgs.bash}/bin/bash";
-        execer = [
-  "cannot:${pkgs.python3.pkgs.brother-ql}/bin/brother_ql"
+        inputs = with pkgs; [
+          curl
+          jq
+          coreutils
+          qrcode
+          imagemagick
+          python3.pkgs.brother-ql
         ];
-      } (builtins.replaceStrings ["logo-v2.svg" ] [ "${./logo-v2.svg}" ] (builtins.readFile ./make.sh));
+        interpreter = "${pkgs.bash}/bin/bash";
+        execer = [ "cannot:${pkgs.python3.pkgs.brother-ql}/bin/brother_ql" ];
+      } (builtins.replaceStrings [ "logo-v2.svg" ] [ "${./logo-v2.svg}" ]
+        (builtins.readFile ./make.sh));
     };
 }
